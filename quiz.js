8bb52e28cef9d947adea847037df750223a82ee9
Rishabh Kant <krrishabhkant@gmail.com>
2026-01-08 16:29:44 +0530

    rendering the options

--- quiz.js
+++ quiz.js
@@ -6,4 +6,25 @@ const questionObj =
       answers: ['Two', 'Three ', 'Four', 'Five'],
       question:
         "How many pieces of bun are in a Mcdonald's Big Mac?",
-    };
+    }
+    ;
+
+
+// const {correctAnswer, answers , question} = questionObj;
+
+// const questionEl = document.getElementById('question');
+// questionEl.textContent = questionObj.question;
+
+
+
+const answers = questionObj.answers;
+
+// const options = document.getElementById('options');
+let totalOptions = [];
+
+for(let i =0; i < answers.length; i++){
+  totalOptions.push(`<button> ${answers[i]} </button>`);
+}
+
+
+console.log(totalOptions);
