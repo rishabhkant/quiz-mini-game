ceb7a60fc8fbc9f34c3de5dc42a774420560ce64
Rishabh Kant <krrishabhkant@gmail.com>
2026-01-09 14:10:56 +0530

    functionality added in buttons

--- quiz.js
+++ quiz.js
@@ -3,28 +3,45 @@ const questionObj =
       category: 'Food & Drink',
       id: 'qa-1',
       correctAnswer: 'Three',
-      answers: ['Two', 'Three ', 'Four', 'Five'],
+      answers: ['Two', 'Three', 'Four', 'Five'],
       question:
         "How many pieces of bun are in a Mcdonald's Big Mac?",
     }
     ;
 
 
-// const {correctAnswer, answers , question} = questionObj;
+const {
+  correctAnswer,
+  answers ,
+  question,
+} = questionObj;
 
-// const questionEl = document.getElementById('question');
-// questionEl.textContent = questionObj.question;
+const questionEl = document.getElementById('question');
+const options = document.getElementById('options');
+const scoreEl = document.getElementById('score');
 
+questionEl.textContent = question;
+let score = 0;
 
+answers.forEach((opt)=>{
+  const btn = document.createElement("button");
+  btn.classList.add('optionButtons')
+  btn.textContent = opt;
+  options.appendChild(btn);
 
-const answers = questionObj.answers;
+  btn.addEventListener('click', ()=>{
+      if(opt === correctAnswer){
+        score++;
+        questionEl.textContent = "Quiz Completed";
+        options.textContent = '';
+      }else{
+        score -= 0.25;
+      }
 
-// const options = document.getElementById('options');
-let totalOptions = [];
+      scoreEl.textContent = `Score: ${score}`;
+      
+  })
+  
+  
+})
 
-for(let i =0; i < answers.length; i++){
-  totalOptions.push(`<button> ${answers[i]} </button>`);
-}
-
-
-console.log(totalOptions);
