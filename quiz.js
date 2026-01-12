734ebe5b207b387d4dab766c633f6a796ac99f64
Rishabh Kant <krrishabhkant@gmail.com>
2026-01-12 23:33:48 +0530

    added multiple questions

--- quiz.js
+++ quiz.js
@@ -1,56 +1,117 @@
-const questionObj = 
-    {
-      category: 'Food & Drink',
-      id: 'qa-1',
-      correctAnswer: 'Three',
-      answers: ['Two', 'Three', 'Four', 'Five'],
-      question:
-        "How many pieces of bun are in a Mcdonald's Big Mac?",
-    };
+// const questionObj = 
+//     {
+//       category: 'Food & Drink',
+//       id: 'qa-1',
+//       correctAnswer: 'Three',
+//       answers: ['Two', 'Three', 'Four', 'Five'],
+//       question:
+//         "How many pieces of bun are in a Mcdonald's Big Mac?",
+//     };
+
+
+const quesJSON = [  
+  {
+    correctAnswer: 'Three ',
+    options: ['Two', 'Three ', 'Four', 'Five'],
+    question:
+      "How many pieces of bun are in a Mcdonald's Big Mac?",
+  },
+  {
+    correctAnswer: 'L. Frank Baum',
+    options: [
+      'Suzanne Collins',
+      'James Fenimore Cooper',
+      'L. Frank Baum',
+      'Donna Leon',
+    ],
+    question:
+      "Which author wrote 'The Wonderful Wizard of Oz'?",
+  },
+  {
+    correctAnswer: 'Atlanta United',
+    options: [
+      'Atlanta United',
+      'Atlanta Impact',
+      'Atlanta Bulls',
+      'Atlanta Stars',
+    ],
+    question:
+      'Which of these is a soccer team based in Atlanta?',
+  },
+  {
+    correctAnswer: 'A Nanny',
+    options: [
+      'A Sow',
+      'A Lioness',
+      'A Hen',
+      'A Nanny',
+    ],
+    question: 'A female goat is known as what?',
+  },
+  {
+    correctAnswer: 'P. L. Travers',
+    options: [
+      'J. R. R. Tolkien',
+      'P. L. Travers',
+      'Lewis Carroll',
+      'Enid Blyton',
+    ],
+    question:
+      "Which author wrote 'Mary Poppins'?",
+  },
+];
 
 
 const {
   correctAnswer,
-  answers ,
+  options,
   question,
-} = questionObj;
+} = quesJSON;
+
+
+
+
+let score = 0;
+let currentQues = 0;
 
 const questionEl = document.getElementById('question');
-const options = document.getElementById('options');
+const optionsEl = document.getElementById('options');
 const scoreEl = document.getElementById('score');
 
-questionEl.textContent = question;
-let score = 0;
 
+function showQues(){
+  questionEl.textContent = question;
 
-
-
-for(let i = answers.length -1; i >= 0 ; i--){
+for(let i = options.length - 1; i >= 0 ; i--){
   let randomNum = Math.floor(Math.random() * (i + 1));
-  let temp = answers[i];
-    answers[i] = answers[randomNum];
-    answers[randomNum] = temp;
+  let temp = options[i];
+    options[i] = options[randomNum];
+    options[randomNum] = temp;
 }
 
-answers.forEach((opt)=>{
+options.forEach((opt)=>{
   const btn = document.createElement("button");
   btn.classList.add('optionButtons');
   btn.textContent = opt;
-  options.appendChild(btn);
+  optionsEl.appendChild(btn);
 
   btn.addEventListener('click', ()=>{
       if(opt === correctAnswer){
         score++;
-        questionEl.textContent = "Quiz Completed";
-        options.textContent = '';
+
       }else{
         score -= 0.25;
       }
 
       scoreEl.textContent = `Score: ${score}`;
       
+      
   })
   
   
 })
+
+}
+
+showQues();
 
